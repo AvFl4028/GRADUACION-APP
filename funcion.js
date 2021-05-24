e6add0e11fcbc88cee6b89e3dfebbb146e73ddbd
AvFl <aviel4028@gmail.com>
2021-05-24 15:02:58 -0500

    backup

--- funcion.js
+++
@@ -1,7 +0,0 @@
-var Name = prompt('Pon tu nombre');
-var Text = 'Bienvenido';
-for (var Name of Name){
-var doc = document.createElement('h2');
-doc.textContent = Name;
-document.body.appendChild(doc);
-}
